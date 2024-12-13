7bb1c47aa15df54b85da5008e5d7ba86e4cfb0e4
Esther <estherifeoluwa091@gmail.com>
2024-12-13 14:47:29 -0800

    Added dotenv for environment variable management, updated server port configuration to use process.env.PORT, and made minor formatting adjustments in db.js connection string.

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const {Pool} = require("pg")
-const connectionString =
-  "postgresql://esther:n1XuKE0ZhsgFIi6KQ3FJjC1SIworbMKb@dpg-cta6ojbtq21c73c11agg-a.oregon-postgres.render.com/campus_events";
+const connectionString ="postgresql://esther:n1XuKE0ZhsgFIi6KQ3FJjC1SIworbMKb@dpg-cta6ojbtq21c73c11agg-a.oregon-postgres.render.com/campus_events";
 
 const pool = new Pool({
     connectionString: connectionString,
--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const authRoutes = require("./routes/authRoutes")
 const eventRoutes = require("./routes/eventRoutes")
-
+require("dotenv").config()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/events",eventRoutes)
-const PORT = 3000
+
+const PORT = process.env.PORT || 3000
+
 app.get('/',(req,res)=>{
     res.send("Welcome to Esther's server")
 })
